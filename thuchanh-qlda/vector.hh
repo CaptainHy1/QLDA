32fe8da53cd7a18276fa37509768ec2b4d7c3b61
CaptainHy1 <khanghy20051038@gmail.com>
2026-03-31 08:34:24 +0700

    fix tv1

--- thuchanh-qlda/vector.hh
+++ thuchanh-qlda/vector.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstddef>
+#include <iostream>
 
 // Moi thanh vien trong nhom tao feature branch, roi lam task duoc
 // giao o comment trong cac function. Thanh vien nhom co the tao
@@ -16,16 +17,44 @@
 class FloatVector {
 public:
         // Thanh vien 1
+        FloatVector::FloatVector() {
+    _capacity = 10;
+    _size = 0;
+    _data = new float[_capacity];
+}
         FloatVector();  // Tao moi array _data bang new hoac malloc().
-        void push(float n); // Them phan tu n vao cuoi vector.
-        float pop();    // Xoa phan tu cuoi cung (phan tu duoc push
-                        // gan day nhat) roi tra ve gia tri phan tu do.
+void FloatVector::_realloc(size_t new_capacity) {
+    float* newData = new float[new_capacity];
+
+    for (size_t i = 0; i < _size; i++) {
+        newData[i] = _data[i];
+    }
+
+    delete[] _data;
+    _data = newData;
+    _capacity = new_capacity;
+}
+
+void FloatVector::push(float n) {
+    if (_size == _capacity) {
+        _realloc(_capacity * 2);
+    }
+    _data[_size++] = n;
+}
+
+float FloatVector::pop() {
+    if (_size == 0) {
+        std::cerr << "Vector rong!\n";
+        return 0;
+    }
+    return _data[--_size];
+}
 
         // Thanh vien 2
         float &operator[](size_t idx);
         float operator[](size_t idx) const;
 
-        // Thanh vien 2 luu y: ham nay thuc hdsdsien deep copy. Thay vi
+        // Thanh vien 2 luu y: ham nay thuc hien deep copy. Thay vi
         // chi gan _data cua FloatVector nay vao _data cua FloatVector
         // kia thi phai copy tung phan tu cua FloatVector kia, de tranh
         // bug double free.
